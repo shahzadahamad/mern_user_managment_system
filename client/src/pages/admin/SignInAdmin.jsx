e45bed9e2d96bcf018a0eaadada5c30372865c1a
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-06-11 22:45:28 +0530

    complete admin sign in functionality

--- client/src/pages/admin/SignInAdmin.jsx
+++ client/src/pages/admin/SignInAdmin.jsx
@@ -1,24 +1,62 @@
+import axios from "../../axios.js";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signInFailure, signInStart, signInSuccess, signInAndUpClear } from "../../redex/admin/adminSlice.js";
 
 
 function SignInAdmin() {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [fromData,setFormData] = useState({});
+  const {error,loading} = useSelector((state) => state.admin);
+
+  const handleChange = (e) => {
+    dispatch(signInAndUpClear());
+    setFormData({...fromData,[e.target.id] : e.target.value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await axios.post('/auth/admin/signin',fromData);
+      dispatch(signInSuccess(res.data));
+      console.log(res.data)
+      navigate('/admin');
+    } catch (error) {
+      dispatch(signInFailure(error.response.data));
+    }
+  }
+
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit} >
         <input
-          type="email"
-          placeholder="Email"
-          id="email"
+          type="text"
+          placeholder="Username"
+          id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign In</button>
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? "Loading..." : "Sign In"}</button>
       </form>
+      {error ? (
+        <p className="text-red-700 mt-5">{error.message || "something went wrong!"}</p>
+      ) : (
+        null
+      )}
     </div>
   )
 }
---
+++ client/src/redex/admin/adminSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentAdmin: null,
+  loading: false,
+  error: false,
+};
+
+const adminSlice = createSlice({
+  name: "admin",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInAndUpClear: (state) => {
+      state.loading = false;
+      state.error = false;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { signInFailure, signInStart, signInSuccess, signInAndUpClear } =
+  adminSlice.actions;
+
+export default adminSlice.reducer;
