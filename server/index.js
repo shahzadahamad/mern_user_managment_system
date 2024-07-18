604ded20a720bcd90b6044965fdee76dcc160fa4
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-07-18 12:41:00 +0530

    add pre production changes .1

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use(express.json());
